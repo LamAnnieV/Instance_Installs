93c51baec0f3518aad0b66e4f8869b8af081c5b0
Annie Lam <139127252+LamAnnieV@users.noreply.github.com>
2023-09-15 18:07:56 -0400

    Create 03_CLI_installs.sh

---
+++ 03_CLI_installs.sh
@@ -0,0 +1,18 @@
+
+
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+
+unzip awscliv2.zip
+
+sudo ./aws/install
+
+aws --version
+
+sleep 3
+echo " "
+
+aws configure
+
+aws ec2 describe-instances
+
+exit 0
