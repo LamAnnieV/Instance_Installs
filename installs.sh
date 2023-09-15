590204e6f68c501d0172ac38f4cdae3a0aedf86b
Annie Lam <139127252+LamAnnieV@users.noreply.github.com>
2023-09-15 11:47:38 -0400

    Update installs.sh

--- installs.sh
+++ installs.sh
@@ -35,22 +35,6 @@ sudo apt upgrade
 sudo apt-get install jenkins
 sudo systemctl start jenkins
 
-echo "Installing CLI..."
-echo " "
-echo "You will need the following information:"
-echo "Access Key: AKIA53MMRRSZNSZ6P36N"
-edho "Secrete Access Key:  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"
-echo "Type: us-east-1"
-echo "Format: json"
-
-sleep 7
-
-curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
-unzip awscliv2.zip
-sudo ./aws/install
-aws --version
-aws configure
-aws ec2 describe-instances
 
 echo " "
 echo "$(curl ifconfig.me | cut -d "u" -f 1):8080"
