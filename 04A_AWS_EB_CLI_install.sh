89b2443e0304880ff92070f625301387034a36e3
Annie Lam <139127252+LamAnnieV@users.noreply.github.com>
2023-09-17 10:48:45 -0400

    Update 04A_AWS_EB_CLI_install.sh

--- 04A_AWS_EB_CLI_install.sh
+++ 04A_AWS_EB_CLI_install.sh
@@ -9,20 +9,6 @@ echo " "
 echo "Signing in as Jenkins user..."
 sudo su - jenkins -s /bin/bash
 
-pip install awsebcli --upgrade --user
 
-export PATH=$PATH:$HOME/.local/bin
-
-eb --version
-
-aws configure
-
-cd workspace/
-
-ls
-
-echo " "
-echo "Manually cd to the pipeline"
-echo " "
 
 exit 0
