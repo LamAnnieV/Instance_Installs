3846cdf8b3332b0f9020c15f23f847069f4f8cb0
Annie Lam <139127252+LamAnnieV@users.noreply.github.com>
2023-09-22 16:56:36 -0400

    Update 04C_AWS_EB_CLI_install.sh

--- 04C_AWS_EB_CLI_install.sh
+++ 04C_AWS_EB_CLI_install.sh
@@ -1,13 +1,23 @@
 #!/bin/bash
 
 echo " "
-echo "**************************************N O T E**********************************"
+echo "***************** N O T E : S C R E E N S H O T - T H I S ***********************"
 echo " "
-
-
-
+echo "For default region, enter '1' for us-east-1"
 echo " "
-echo "**************************************N O T E**********************************"
+echo "For application to use, press enter"
+echo "For Enter Application Name, press enter"
+echo "It appears you are using Python. Is this correct?, press enter"
+echo "For echo "For platform branch, enter option for Python 3.9"
+echo "For code commit, enter 'n'"
+
+echo "For do you want to set up ssh, enter 'n'"
+echo "For Environment name, press enter"
+echo "For DNS CNAME prefex, press enter"
+echo "For load balancer type, press enter"
+echo "For spot fleet, enter 'n'"
+echo " "
+echo "************************************** N O T E **********************************"
 echo " "
 
 
