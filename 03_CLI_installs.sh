d28ba9dbbc9d1af2ea28c774d20ebc0bbc91f894
Annie Lam <139127252+LamAnnieV@users.noreply.github.com>
2023-09-22 14:48:16 -0400

    Update 03_CLI_installs.sh

--- 03_CLI_installs.sh
+++ 03_CLI_installs.sh
@@ -2,17 +2,19 @@
 
 
 echo " "
-echo "Installing AWS CLI..."
 
+echo "**************************************N O T E**********************************"
 echo " "
 echo "Please have your AWS Access Key ID and AWS Secret Access Key ready"
 echo "When prompt to enter Default region name, enter 'us-east-1'"
 echo "When prompt to enter Default output format, enter 'json'"
-sleep 3
+echo " "
+echo "**************************************N O T E**********************************"
+echo " "
 echo "Press Enter to continue..."
 read -r
-
-
+echo " "
+echo "Installing AWS CLI..."
 curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
 
 unzip awscliv2.zip
