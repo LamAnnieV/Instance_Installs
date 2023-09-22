06b0b35d07bbedac26fbe5c376f868d54dc75f0f
Annie Lam <139127252+LamAnnieV@users.noreply.github.com>
2023-09-22 14:17:22 -0400

    Update 04B_AWS_EB_CLI_install.sh

--- 04B_AWS_EB_CLI_install.sh
+++ 04B_AWS_EB_CLI_install.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+echo " "
+echo "Make sure your EB Roles are configured"
+echo "Please have your AWS Access Key ID and AWS Secret Access Key ready"
+echo "When prompt to enter Default region name, enter 'us-east-1'"
+echo "When prompt to enter Default output format, enter 'json'"
+sleep 3
+echo "Press Enter to continue..."
+read -r
+
 pip install awsebcli --upgrade --user
 
 export PATH=$PATH:$HOME/.local/bin
