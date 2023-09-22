fad1132fdd157a239f82f5cf2f7a5017d4f007ce
Annie Lam <139127252+LamAnnieV@users.noreply.github.com>
2023-09-22 14:43:14 -0400

    Update 04B_AWS_EB_CLI_install.sh

--- 04B_AWS_EB_CLI_install.sh
+++ 04B_AWS_EB_CLI_install.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 echo " "
-echo "**************************************N O T E**********************************
+echo "**************************************N O T E**********************************"
 echo "Make sure your EB Roles are configured"
 echo "Please have your AWS Access Key ID and AWS Secret Access Key ready"
 echo "When prompt to enter Default region name, enter 'us-east-1'"
 echo "When prompt to enter Default output format, enter 'json'"
-echo "**************************************N O T E**********************************
+echo "**************************************N O T E**********************************"
 sleep 3
 echo "Press Enter to continue..."
 read -r
