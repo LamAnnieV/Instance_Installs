58ad62a36522b3bbdd3e368e6a6d28b66858d580
Annie Lam <139127252+LamAnnieV@users.noreply.github.com>
2023-09-28 19:03:48 -0400

    Update 02_other_installs.sh

--- 02_other_installs.sh
+++ 02_other_installs.sh
@@ -20,19 +20,16 @@ sudo apt install python3.10-venv
 echo "Installing python-pip..."
 sleep 2
 sudo apt update
-sudo apt upgrade
 sudo apt install python-pip
 
 echo "Installing python3-pip..."
 sleep 2
 sudo apt update
-sudo apt upgrade
 sudo apt install python3-pip
 
 echo "Installing unzip..."
 sleep 2
 sudo apt update
-sudo apt upgrade
 sudo apt install unzip
 
 exit 0
