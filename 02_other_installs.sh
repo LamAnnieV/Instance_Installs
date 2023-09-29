43ffbaf409d9214a68830b9942bd38065180f967
Annie Lam <139127252+LamAnnieV@users.noreply.github.com>
2023-09-28 21:51:13 -0400

    Update 02_other_installs.sh

--- 02_other_installs.sh
+++ 02_other_installs.sh
@@ -1,14 +1,6 @@
 #!/bin/bash
 
-echo " "
-echo "************************************** N O T E **********************************"
-echo " "
-echo "For all the prompts in the installs, press 'Enter'"
-echo " "
-echo "************************************** N O T E **********************************"
-echo " "
-echo "Press Enter to continue..."
-read -r
+
 echo " "
 echo "Installing python3.10-venv..."
 sleep 2
@@ -18,14 +10,19 @@ sudo apt upgrade
 sudo apt install -y python3.10-venv
 
 
-echo "Installing -y python3-pip..."
+echo "Installing python3-pip..."
 sleep 2
 sudo apt update
-sudo apt install python3-pip
+sudo apt install -y python3-pip
 
 echo "Installing unzip..."
 sleep 2
 sudo apt update
 sudo apt install -y unzip
+
+echo "Installing ngnix..."
+sleep 2
+sudo apt update
+sudo apt install -y ngnix
 
 exit 0
