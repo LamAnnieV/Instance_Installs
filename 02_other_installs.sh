6c0d10869100e24c7ef14d197005f06eb1d436fc
Annie Lam <139127252+LamAnnieV@users.noreply.github.com>
2023-09-28 21:55:27 -0400

    Update 02_other_installs.sh

--- 02_other_installs.sh
+++ 02_other_installs.sh
@@ -1,28 +1,24 @@
 #!/bin/bash
 
-
-echo " "
-echo "Installing python3.10-venv..."
-sleep 2
-
+echo "Running updates...
 sudo apt update
 sudo apt upgrade
+
+echo " "
+echo "Installing python3.10-venv...
 sudo apt install -y python3.10-venv
 
-
+echo " "
 echo "Installing python3-pip..."
-sleep 2
-sudo apt update
 sudo apt install -y python3-pip
 
+echo " "
 echo "Installing unzip..."
-sleep 2
-sudo apt update
 sudo apt install -y unzip
 
+
+echo " "
 echo "Installing ngnix..."
-sleep 2
-sudo apt update
 sudo apt install -y ngnix
 
 exit 0
