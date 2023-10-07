18126f524e184a378436c7416d3b57ef3c4856e3
Annie Lam <139127252+LamAnnieV@users.noreply.github.com>
2023-10-07 10:38:22 -0400

    Update 01_jenkins_installs.sh

--- 01_jenkins_installs.sh
+++ 01_jenkins_installs.sh
@@ -7,7 +7,7 @@ sudo apt -y upgrade
 
 echo " "
 echo "Installing Jenkins..."
-sudo apt-get install fontconfig openjdk-17-jre
+sudo apt-get install -y fontconfig openjdk-17-jre
 curl -fsSL https://pkg.jenkins.io/debian/jenkins.io-2023.key | sudo tee /usr/share/keyrings/jenkins-keyring.asc > /dev/null
 echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] https://pkg.jenkins.io/debian binary/ | sudo tee /etc/apt/sources.list.d/jenkins.list > /dev/null
 sudo apt-get install -y jenkins
