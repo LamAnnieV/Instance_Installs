ec2118ec9cae6b4b935fe5f7aa36ef8e041a8f92
Annie Lam <139127252+LamAnnieV@users.noreply.github.com>
2023-10-27 12:42:32 -0400

    Update 03_CLI_installs.sh

--- 03_CLI_installs.sh
+++ 03_CLI_installs.sh
@@ -16,6 +16,8 @@ echo " "
 echo "Installing AWS CLI..."
 curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
 
+sudo apt install unzip
+
 unzip awscliv2.zip
 
 sudo ./aws/install
